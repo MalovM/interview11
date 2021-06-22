3714365a23267bcc02f5d1fdc940191ef245f4b4
Max <maximmalov.prog@gmail.com>
2021-06-22 16:29:14 +0500

    working with animation and css

--- JS/index.js
+++ JS/index.js
@@ -50,7 +50,8 @@ function init() {
      */
     function getCoordinates(time) {
         // TODO: implement body of this function
-        return [0.0, 1.0];
+
+        return [Math.sin(time), Math.cos(time)];
     }
 
     /**
